9930c02bffbbf082b4848e9f1c05aea0d79a43c7
fatoumata_coulibaly <fatoumatacoulibaly4155@gmail.com>
2022-11-02 09:03:22 +0000

    Contenu de la page text et icon

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:video_player/video_player.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(const MyApp());
@@ -88,13 +89,19 @@ class HomePage extends StatelessWidget {
   //on crée une var de type list qui va contenir des maps et toute nos infos
   final List<Map> tiktokitems = [
     {
-      "video": "img/videos/vid1.MOV",
+      "video": "img/videos/vid1.mp4",
     },
     {
-      "video": "img/videos/vid2.MOV",
+      "video": "img/videos/vid2.mp4",
     },
     {
-      "video": "img/videos/vid3.MOV",
+      "video": "img/videos/vid3.mp4",
+    },
+    {
+      "video": "img/videos/vid1.mp4",
+    },
+    {
+      "video": "img/videos/vid2.mp4",
     },
   ];
 
@@ -114,8 +121,13 @@ class HomePage extends StatelessWidget {
             return Container(
               color: const Color(0xFF141518),
               child: Stack(
-                children: const [VideoWidget()],
+                children:  [VideoWidget(videoUrl: item['video'],
+                ),
+                  const PostContent()
+                ],
+
               ),
+
               //color: item['color'],
               // child: const Center(
               //   child: Text('text'),
@@ -138,20 +150,29 @@ class HomePage extends StatelessWidget {
 }
 
 class VideoWidget extends StatefulWidget {
-  const VideoWidget({super.key});
+   const VideoWidget({Key? key, required this.videoUrl}) : super(key: key);
+
+  //const VideoWidget({super.key});
+  final String videoUrl;
+
 
   @override
-  State<VideoWidget> createState() => _VideoWidgetState();
+  State<VideoWidget> createState() => _VideoWidgetState(this.videoUrl);
 }
 
 class _VideoWidgetState extends State<VideoWidget> {
   //late pour dire quon va l'initialiser apres
   late VideoPlayerController _controller;
+  final String videoUrl;
+  _VideoWidgetState(this.videoUrl);
+
   void initState() {
     super.initState();
 
-    _controller = VideoPlayerController.asset('img/videos/vid1.MOV')
+    _controller = VideoPlayerController.asset(videoUrl)
       ..initialize().then((_) {
+        //pour lncer chacune des videos
+        _controller.play();
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
       });
@@ -162,3 +183,248 @@ class _VideoWidgetState extends State<VideoWidget> {
     return VideoPlayer(_controller);
   }
 }
+class PostContent extends StatelessWidget {
+  const PostContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //une collonne avec é child
+    return Column(
+      children: [
+        Container(
+          height: 100,
+          padding: EdgeInsets.only(top: 40),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Following',
+                style: TextStyle(
+                  color: Colors.white54,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(width: 20),
+              Text(
+                'For you',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Container(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    //
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      //afficher horizontalment
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '@extremesports_95',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Goein full send in Squaw Valley. #snow @snowboarding # extremesports #sendit #winter',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.music_note,
+                              color: Colors.white,
+                              size: 15,
+                            ),
+                            SizedBox(width: 5),
+                            Text(
+                              'Original Sound - extremesports_95',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 80,
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        //stack por superposer des widgets
+                        child: Stack(
+                          //place licon + sur l'imge
+                          alignment: AlignmentDirectional.bottomCenter,
+                          children: [
+                            Container(
+                              //pcer le cor cle avatar dns un cont et lui donner une mrge
+                              margin: EdgeInsets.only(bottom: 10),
+                              child: CircleAvatar(
+                                radius: 25,
+                                backgroundImage: AssetImage('img/imagess/orange.jpeg',),
+                              ),
+                            ),
+                            //cont pour le button add du profil
+                            Container(
+                              padding: EdgeInsets.all(2),
+                              decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Icon(
+                                Icons.add,
+                                color: Colors.white,
+                                size: 15,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      //favoris icon et text
+                      Container(
+                        height: 80,
+                        child: Column(
+                          children: [
+                            Icon(
+                              Icons.favorite,
+                              color: Colors.white.withOpacity(0.85),
+                              size: 45,
+                            ),
+                            Text(
+                              '25.0K',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 80,
+                        child: Column(
+                          children: [
+                            Icon(
+                              Icons.comment,
+                              color: Colors.white.withOpacity(0.85),
+                              size: 45,
+                            ),
+                            Text(
+                              '156',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 80,
+                        child: Column(
+                          children: [
+                            Icon(
+                              Icons.share,
+                              color: Colors.white.withOpacity(0.85),
+                              size: 45,
+                            ),
+                            Text(
+                              '123',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      AnimatedLogo(),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+class AnimatedLogo extends StatefulWidget {
+  const AnimatedLogo({Key? key}) : super(key: key);
+
+  @override
+  State<AnimatedLogo> createState() => _AnimatedLogoState();
+}
+
+class _AnimatedLogoState extends State<AnimatedLogo>   with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  @override
+  void initState() {
+    //intitiaalisation du controller pour effectuer la transition
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 4000),
+      vsync: this,
+    );
+    //repeat l'nimation pres l durée definis
+    _controller.repeat();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (_, child) {
+        return Transform.rotate(
+          //appliquer cette rotation
+          angle: _controller.value * 2 * math.pi,
+          child: child,
+        );
+      },
+      child: Container(
+        height: 45,
+        width: 45,
+        padding: EdgeInsets.all(5),
+        //boite contenant les 2 images
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          image: DecorationImage(
+            image: AssetImage("img/imagess/disctiktok.jpeg"),
+          ),
+        ),
+        child: Image.asset('img/imagess/logotiktok.png'),
+      ),
+    );
+  }
+}
+
+
